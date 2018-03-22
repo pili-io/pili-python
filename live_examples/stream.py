5ea292305e14890158d75608daef5b54acfb3a36
bernieyangmh <berniey@163.com>
2018-03-22 13:29:20 +0800

    add stream

--- live_examples/stream.py
+++ live_examples/stream.py
@@ -8,21 +8,16 @@ access_key = "..."
 # 替换成自己 Qiniu 账号的 SecretKey
 secret_key = "..."
 
-hub_name = '...'
+hub_name = "..."
+
+stream_name = "..."
 
 mac = Mac(access_key, secret_key)
 
 hub = Hub(mac, hub_name)
 
-liveonly = False
+stream = hub.get(stream_name)
 
-prefix = ""
+print(stream)
 
-limit = 100
-
-resp = hub.list()
-
-print(resp.status_code)
-print(resp.headers)
-print(resp.text)
 
