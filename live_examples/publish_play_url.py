d6030be6d66deac78c35e626d9fbd8bfbf71debf
bernieyangmh <berniey@163.com>
2018-03-22 11:13:11 +0800

    fix url..

--- live_examples/publish_play_url.py
+++ live_examples/publish_play_url.py
@@ -8,11 +8,11 @@ access_key = "..."
 # 替换成自己 Qiniu 账号的 SecretKey
 secret_key = "..."
 
-domain = '...'
-
 hub_name = '...'
 
-stream_name = '...'
+domain = '...'
+
+stream_name = '123'
 
 expire = 3600
 
@@ -22,13 +22,17 @@ hub = Hub(mac, hub_name)
 
 stream = hub.get(stream_name)
 
-
+"""
+该生成推流地址方法只适用于v2版本的限时鉴权，v2的无鉴权只需拼接域名，直播空间名和流名，v1版本的鉴权算法见下
+https://developer.qiniu.com/pili/kb/1332/broadcast-authentication-mode
+https://developer.qiniu.com/pili/kb/2635/seven-cows-live-push-flow-authentication-md
+"""
 print rtmp_publish_url(domain, hub_name, stream_name, mac, expire)
 
-print rtmp_play_url(domain, hub_name, stream_name)
-
-print hls_play_url(domain, hub_name, stream_name)
-
-print hdl_play_url(domain, hub_name, stream_name)
-
-print snapshot_play_url(domain, hub_name, stream_name)
+# print rtmp_play_url(domain, hub_name, stream_name)
+#
+# print hls_play_url(domain, hub_name, stream_name)
+#
+# print hdl_play_url(domain, hub_name, stream_name)
+#
+# print snapshot_play_url(domain, hub_name, stream_name)
